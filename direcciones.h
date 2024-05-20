85f952cccd0cc1ff235aec70a3a4aa35dc09c84a
Luis Q <luis.alberto31.66@gmail.com>
2024-05-20 00:06:10 -0300

    cree una rama de prueba

---
+++ direcciones.h
@@ -0,0 +1,14 @@
+#ifndef DIRECCIONES_H_INCLUDED
+#define DIRECCIONES_H_INCLUDED
+
+typedef struct{
+
+    char provincia[];
+    char localidad[];
+    char calle[];
+    int altura;
+
+
+}stDirecciones;
+
+#endif // DIRECCIONES_H_INCLUDED
